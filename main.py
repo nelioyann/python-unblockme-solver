c02b86323819b719ab9b444e6ce092deb6daf225
nelioyann <nanayannick1998@gmail.com>
2019-04-05 19:50:19 +0200

    direct instances, no variables

--- main.py
+++ main.py
@@ -211,9 +211,9 @@ class Blocs:
 # getattr(Board, "bloctest")
 # Obstables
 # Easy set
-bloc_1 = Blocs([1, 1], [1, 2])
-bloc_2 = Blocs([0, 3], [1, 3])
-bloc_3 = Blocs([2, 2], [2, 3])
+Blocs([1, 1], [1, 2])
+Blocs([0, 3], [1, 3])
+Blocs([2, 2], [2, 3])
 # bloc_4 = Blocs([3, 1], [3, 2])
 
 
